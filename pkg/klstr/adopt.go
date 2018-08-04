91d9e98b1eacdde9a8dcb782f4c6c851796f8796
vagmi <vagmi.mudumbai@gmail.com>
2018-08-04 12:07:04 +0530

    install grafana if it does not exist

--- pkg/klstr/adopt.go
+++ pkg/klstr/adopt.go
@@ -1,11 +1,10 @@
 package klstr
 
 import (
+	"os"
+
+	"github.com/klstr/klstr/pkg/klstr/manifests"
 	log "github.com/sirupsen/logrus"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -17,6 +16,9 @@ type AdoptOptions struct {
 }
 
 func AdoptCluster(ao *AdoptOptions) {
+	if ao.KubeConfig == "" {
+		ao.KubeConfig = os.Getenv("KUBECONFIG")
+	}
 	config, err := clientcmd.BuildConfigFromFlags("", ao.KubeConfig)
 	if err != nil {
 		log.Errorf("Unable to setup client config - %s", err.Error())
@@ -27,39 +29,8 @@ func AdoptCluster(ao *AdoptOptions) {
 		log.Errorf("Unable to create client from config - %s", err.Error())
 		panic(err)
 	}
-	selector := labels.NewSelector()
-	req, err := labels.NewRequirement("app", selection.Equals, []string{"helm"})
+	err = manifests.GetGrafanaDeployment(clientSet, "aws")
 	if err != nil {
-		log.Errorf("Unable to create filter for helm - %s", err.Error())
 		panic(err)
-	}
-	selector = selector.Add(*req)
-	log.Info(selector.String())
-	pods, err := clientSet.CoreV1().Pods("kube-system").List(metav1.ListOptions{
-		LabelSelector: selector.String(),
-	})
-
-	pod, err := clientSet.CoreV1().Pods("default").Create(&corev1.Pod{
-		TypeMeta: metav1.TypeMeta{
-			Kind: "Pod",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   "klstr",
-			Labels: map[string]string{"app": "klstr"},
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				corev1.Container{Name: "klstr", Image: "quay.io/klstr/klstr:latest", Args: []string{"loop"}},
-			},
-		},
-	})
-	if err != nil {
-		log.Infof("Unable to create pod - %v", err)
-	} else {
-		log.Infof("Pod created %s", pod.GetName())
-	}
-
-	for _, p := range pods.Items {
-		log.Infof("Found pod %s", p.Name)
 	}
 }
